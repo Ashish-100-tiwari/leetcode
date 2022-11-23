7e87a8aff2d4aa385c893ca0aec05c4833402d4f
Ashish Tiwari <80841006+Ashish-100-tiwari@users.noreply.github.com>
2022-11-23 09:23:16 +0530

    Added solution - LeetHub

---
+++ Koko Eating Bananas - GFG/koko-eating-bananas.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int Solve(int N, vector<int>& piles, int H) {
+        // Code here
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int t= functime(mid,piles);
+            if(t<=H){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+            
+        }
+        return ans;
+        
+    }
+    int functime(int mid,vector<int>&piles){
+        int sum=0;
+        for(int i=0;i<piles.size();i++){
+            sum+=(piles[i]/mid)+((piles[i]%mid)!=0);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        int h;
+        cin >> h;
+        Solution obj;
+
+        cout << obj.Solve(n, a, h);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
